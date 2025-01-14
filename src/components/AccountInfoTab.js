aa4226115932ffb3c375416c20ccba88e6cb88eb
gfxv <superfreezu@gmail.com>
2025-01-14 18:57:33 +0300

    feat: add working user info display (with update name)

--- src/components/AccountInfoTab.js
+++ src/components/AccountInfoTab.js
@@ -1,135 +1,212 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useContext } from "react";
+import axios from "axios";
+
+import API_URL from "../config";
+import { AuthContext } from "../context/AuthContext";
 
 const AccountInfoTab = () => {
+  const [error, setError] = useState("");
   const [isEditing, setIsEditing] = useState(false);
+  const [currentName, setCurrentName] = useState("");
+  const [userId, setUserId] = useState(-1);
   const [username, setUsername] = useState("JohnDoe");
-  const [email, setEmail] = useState("johndoe@example.com");
-  const [password, setPassword] = useState("");
-  const [balance, setBalance] = useState(1000); // Example balance
+  // const [email, setEmail] = useState("johndoe@example.com");
+  // const [password, setPassword] = useState("");
+  const [gamesHosted, setGamesHosted] = useState(0);
+  const [gamesPlayed, setGamesPlayed] = useState(0);
+  const [gamesWon, setGamesWon] = useState(0);
+  const [gamesLost, setGamesLost] = useState(0);
+  const [balance, setBalance] = useState(0);
   const [paymentMethod, setPaymentMethod] = useState("");
   const [bankCard, setBankCard] = useState("");
 
+  const { authState, login } = useContext(AuthContext);
+  const token = authState.token;
+
   const handleTopUp = () => {
-    // Logic to top up the balance
     alert("Balance topped up!");
   };
 
-  const handleSave = () => {
-    // Logic to save the changes
-    setIsEditing(false);
-    alert("Profile updated!");
+  const handleUpdateUsername = async () => {
+    if (currentName === username) {
+      return;
+    }
+    const data = {
+      new_username: username,
+    }
+    
+    try {
+      const response = await axios.patch(`${API_URL}/user/update_username`, data, {
+          headers: {
+            Authorization: `Bearer ${token}`
+          },
+        }
+      );
+      if (response.data.status.toLowerCase() === 'success') {
+        setCurrentName(username);
+        setIsEditing(false);
+        setError('');
+        login(token, authState.id, username, authState.balance)
+      } else {
+        alert(response.data.message || 'Failed to update username');
+      }
+    } catch (err) {
+      alert('An error occurred while updating the username');
+      console.error(err);
+    }
   };
+
+  useEffect(() => {
+    const fetchUserInfo = async () => {
+      try {
+        const response = await axios.get(`${API_URL}/user/get_user_info`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        if (response.data.status.toLowerCase() === "success") {
+          setUserId(response.data.user.id);
+          setUsername(response.data.user.username);
+          setBalance(response.data.user.balance);
+          setGamesHosted(response.data.usersStats.games_hosted);
+          setGamesPlayed(response.data.usersStats.games_played);
+          setGamesWon(response.data.usersStats.wins);
+          setGamesLost(response.data.usersStats.losses);
+
+          setCurrentName(response.data.user.username); // for better update_name logic
+        } else {
+          setError(
+            response.data.message || "Failed to fetch your account info"
+          );
+        }
+      } catch (err) {
+        setError("An error occurred while fetching account info");
+        console.error(err);
+      }
+    };
+
+    fetchUserInfo();
+  }, []);
 
   return (
     <div className="flex">
-      <div className="w-1/2 pr-4">
-        <div className="flex justify-between items-center mb-4">
-          <h2 className="text-xl font-bold">Account Information</h2>
-          <button
-            onClick={() => setIsEditing(true)}
-            className="bg-blue-500 text-white px-3 py-1 rounded"
-          >
-            Edit
-          </button>
+      {error && (
+        <div className="my-4 p-2 bg-red-100 border border-red-400 text-red-700 rounded">
+          {error}
         </div>
-        <div className="mb-2">
-          <div className="flex items-center">
-            <label className="w-1/4 mr-2">Username</label>
-            {isEditing ? (
-              <input
-                type="text"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                className="border p-1 w-full"
-              />
-            ) : (
-              <strong>{username}</strong>
+      )}
+
+      {!error && (
+        <div className="w-1/2 pr-4">
+          <div className="flex justify-between items-center mb-4">
+            <h2 className="text-xl font-bold">Account Information</h2>
+            <button
+              onClick={() => setIsEditing(true)}
+              className="bg-blue-500 text-white px-3 py-1 rounded"
+            >
+              Edit
+            </button>
+          </div>
+          <div className="mb-3">
+            <div className="flex items-center">
+              <label className="w-1/3 mr-2">Id</label>
+              <strong>{userId}</strong>
+            </div>
+            <div className="flex items-center">
+              <label className="w-1/3 mr-2">Username</label>
+              {isEditing ? (
+                <input
+                  type="text"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  className="border p-1 w-fit h-fit"
+                />
+              ) : (
+                <strong>{username}</strong>
+              )}
+            </div>
+          </div>
+          <div className="mb-2">
+            <div className="flex items-center">
+              <label className="w-1/3 mr-2">Games hosted</label>
+              <strong>{gamesHosted}</strong>
+            </div>
+            <div className="flex items-center">
+              <label className="w-1/3 mr-2">Games Played</label>
+              <strong>{gamesPlayed}</strong>
+            </div>
+            <div className="flex items-center">
+              <label className="w-1/3 mr-2">Wins</label>
+              <strong>{gamesWon}</strong>
+            </div>
+            <div className="flex items-center">
+              <label className="w-1/3 mr-2">Losses</label>
+              <strong>{gamesLost}</strong>
+            </div>
+
+            {gamesPlayed != 0 && (
+              <div className="flex items-center">
+                <label className="w-1/3 mr-2">Winrate</label>
+                <strong>{gamesWon / gamesPlayed}</strong>
+              </div>
+            )}
+
+            {gamesPlayed === 0 && (
+              <div className="flex items-center">
+                <label className="w-1/3 mr-2">Winrate</label>
+                <strong>No data</strong>
+              </div>
             )}
           </div>
+          {isEditing && (
+            <button
+              onClick={handleUpdateUsername}
+              className="bg-green-500 text-white p-2 rounded w-full"
+            >
+              Save
+            </button>
+          )}
         </div>
-        <div className="mb-2">
-          <div className="flex items-center">
-            <label className="w-1/4 mr-2">Email</label>
-            {isEditing ? (
-              <input
-                type="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                className="border p-1 w-full"
-              />
-            ) : (
-              <strong>{email}</strong>
-            )}
+      )}
+
+      {!error && (
+        <div className="w-1/2 pl-4">
+          <h2 className="text-xl font-bold mb-4">Current Balance</h2>
+          <div className="mb-4">
+            <strong>Balance:</strong> ${balance}
+          </div>
+          <div className="mb-4">
+            <label className="block mb-2">Payment Method</label>
+            <select
+              value={paymentMethod}
+              onChange={(e) => setPaymentMethod(e.target.value)}
+              className="border p-2 w-full"
+            >
+              <option value="">Select Payment Method</option>
+              <option value="creditCard">Credit Card</option>
+              <option value="paypal">PayPal</option>
+            </select>
+          </div>
+          <div className="mb-4">
+            <label className="block mb-2">Bank Card</label>
+            <input
+              type="text"
+              value={bankCard}
+              onChange={(e) => setBankCard(e.target.value)}
+              className="border p-2 w-full"
+              placeholder="Enter Bank Card Number"
+            />
+          </div>
+          <div className="w-full flex justify-end">
+            <button
+              onClick={handleTopUp}
+              className="bg-green-500 text-white mt-4 p-2 rounded w-1/3"
+            >
+              Top Up Balance
+            </button>
           </div>
         </div>
-        <div className="mb-2">
-          <div className="flex items-center">
-            <label className="w-1/4 mr-2">Password</label>
-            {isEditing ? (
-              <input
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                className="border p-1 w-full"
-              />
-            ) : (
-              <strong>********</strong>
-            )}
-          </div>
-        </div>
-        <div className="mb-2">
-          <strong>Game Statistics:</strong>
-          <ul>
-            <li>Total Games Played: 50</li>
-            <li>Wins: 30</li>
-            <li>Losses: 20</li>
-          </ul>
-        </div>
-        {isEditing && (
-          <button
-            onClick={handleSave}
-            className="bg-green-500 text-white p-2 rounded w-full"
-          >
-            Save
-          </button>
-        )}
-      </div>
-      <div className="w-1/2 pl-4">
-        <h2 className="text-xl font-bold mb-4">Current Balance</h2>
-        <div className="mb-4">
-          <strong>Balance:</strong> ${balance}
-        </div>
-        <div className="mb-4">
-          <label className="block mb-2">Payment Method</label>
-          <select
-            value={paymentMethod}
-            onChange={(e) => setPaymentMethod(e.target.value)}
-            className="border p-2 w-full"
-          >
-            <option value="">Select Payment Method</option>
-            <option value="creditCard">Credit Card</option>
-            <option value="paypal">PayPal</option>
-          </select>
-        </div>
-        <div className="mb-4">
-          <label className="block mb-2">Bank Card</label>
-          <input
-            type="text"
-            value={bankCard}
-            onChange={(e) => setBankCard(e.target.value)}
-            className="border p-2 w-full"
-            placeholder="Enter Bank Card Number"
-          />
-        </div>
-        <div className="w-full flex justify-end">
-          <button
-            onClick={handleTopUp}
-            className="bg-green-500 text-white mt-4 p-2 rounded w-1/3"
-          >
-            Top Up Balance
-          </button>
-        </div>
-      </div>
+      )}
     </div>
   );
 };
