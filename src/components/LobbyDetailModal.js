f8ea68ba9bf1c508898e889ba35da752e8ff3d8e
gfxv <superfreezu@gmail.com>
2025-01-16 09:58:56 +0300

    feat: start game button and table status

--- src/components/LobbyDetailModal.js
+++ src/components/LobbyDetailModal.js
@@ -6,15 +6,36 @@ import { Stomp } from "@stomp/stompjs";
 
 import { API_URL, WS_URL } from "../config";
 import { AuthContext } from "../context/AuthContext";
+import { translateCard } from "../utils/cardsMapper";
 
 const LobbyDetailModal = ({ isOpen, onClose, lobby, onJoin }) => {
+  const { authState } = useContext(AuthContext);
+  const token = authState.token;
+  const userId = authState.id;
+
+  const [isOwner, setIsOwner] = useState(lobby.host_id.id === userId);
+  const [canStart, setCanStart] = useState(false);
   const navigate = useNavigate();
   const [tables, setTables] = useState([]);
   const [error, setError] = useState("");
   const stompClientRef = useRef(null);
 
-  const { authState } = useContext(AuthContext);
-  const token = authState.token;
+  const visibilityColor = (table) => {
+    switch (table.status.toLowerCase()) {
+      case "not_started":
+        return "bg-blue-400"
+      case "started":
+        return "bg-emerald-400"
+      case "end":
+        return "bg-gray-400"
+      default:
+        return "bg-red-400"
+    }
+  }
+
+  const handleStartGame = async () => {
+
+  }
 
   const exitLobby = async () => {
     try {
@@ -56,46 +77,70 @@ const LobbyDetailModal = ({ isOpen, onClose, lobby, onJoin }) => {
     }
   }
 
+  const fetchLobbyAndTables = async () => {
+    try {
+      const tablesResponse = await axios.post(`${API_URL}/lobby/get_tables`, {
+        lobby_id: lobby.id,
+      });
 
+      if (tablesResponse.data.status.toLowerCase() === "success") {
+        const ts = tablesResponse.data.tables;
+        for (const table of ts) {
+          const usersResponse = await axios.post(`${API_URL}/table/get_users`, {
+              table_id: table.id,
+            }
+          );
+          if (usersResponse.data.status.toLowerCase() === "success") {
+            table.players_joined = usersResponse.data.users.length;
+          } else {
+            setError(usersResponse.data.message || "Failed to fetch users");
+            return;
+          }
+        }
+        setTables(ts);
+      } else {
+        setError(tablesResponse.data.message || "Failed to fetch tables");
+      }
+    } catch (err) {
+      setError("An error occurred while fetching data");
+      console.error(err);
+    }
+  };
+
+  const canStartGame = async () => {
+    for (const table of tables) {
+      if (table.players_joined < lobby.min_players) {
+        return false;
+      }
+    }
+    return true;
+  }
 
   useEffect(() => {
-    const fetchLobbyAndTables = async () => {
-      try {
-        const tablesResponse = await axios.post(`${API_URL}/lobby/get_tables`, {
-          lobby_id: lobby.id,
-        });
-
-        if (tablesResponse.data.status.toLowerCase() === "success") {
-          const ts = tablesResponse.data.tables;
-          for (const table of ts) {
-            const usersResponse = await axios.post(`${API_URL}/table/get_users`, {
-                table_id: table.id,
-              }
-            );
-            if (usersResponse.data.status.toLowerCase() === "success") {
-              table.players_joined = usersResponse.data.users.length;
-            } else {
-              setError(usersResponse.data.message || "Failed to fetch users");
-              return;
-            }
-          }
-          setTables(ts);
-        } else {
-          setError(tablesResponse.data.message || "Failed to fetch tables");
-        }
-      } catch (err) {
-        setError("An error occurred while fetching data");
-        console.error(err);
-      }
-    };
     fetchLobbyAndTables();
+    canStartGame().then((val) => setCanStart(val))
+    console.log(canStart)
   }, [lobby]);
+
+  const processEvent = async (message) => {
+    switch (message.event) {
+      case "ENTER":
+        await fetchLobbyAndTables();
+        setCanStart(canStartGame)
+        break;
+      case "EXIT":
+        await fetchLobbyAndTables();
+        setCanStart(canStartGame)
+      default:
+        break;
+    }
+  }
 
   useEffect(() => {
     stompClientRef.current = Stomp.over(() => new SockJS(WS_URL));
     stompClientRef.current.connect({}, (frame) => {
       stompClientRef.current.subscribe("/topic/lobby", (message) => {
-        console.log(JSON.parse(message.body));
+        processEvent(JSON.parse(message.body));
       });
     });
 
@@ -120,9 +165,6 @@ const LobbyDetailModal = ({ isOpen, onClose, lobby, onJoin }) => {
       );
 
       if (response.data.status.toLowerCase() === "success") {
-
-        console.log("1. enter lobby - success")
-
         const tableEnterResponse = await axios.post(`${API_URL}/lobby/enter_table`, {
           table_id: tableId,
         }, {
@@ -131,25 +173,19 @@ const LobbyDetailModal = ({ isOpen, onClose, lobby, onJoin }) => {
           },
         });
         if (tableEnterResponse.data.status.toLowerCase() === "success") {
-          
-          console.log("2. enter table - success")
-
           navigate(`/lobby/${lobby.id}/${tableId}`);
         } else {
-          console.log("3. enter table - fail, exiting")
           await exitTabe();
           await exitLobby()
           setError(tableEnterResponse.data.message || "Failed to join table")
         }
 
       } else {
-        console.log("4. enter lobby - fail, exiting")
         await exitTabe();
         await exitLobby()
         setError(response.data.message || "Failed to join lobby")
       }
     } catch (err) {
-      console.log("ERRRRRRR");
       console.error(err);
       return null;
     }
@@ -187,6 +223,25 @@ const LobbyDetailModal = ({ isOpen, onClose, lobby, onJoin }) => {
 
         { error && (<div className="mb-4 text-red-500">Error: {error}</div>)}
 
+        {isOwner && (
+          canStart ? (
+            <button
+            onClick={handleStartGame}
+            className="bg-green-500 mb-4 text-white p-2 rounded w-full"
+            >
+              Start Game
+            </button>
+          ) : (
+            <button
+            className="bg-gray-500 mb-4 cursor-default text-white p-2 rounded w-full"
+            >
+              Start Game
+            </button>
+          )
+        )}
+        
+
+
         {tables.length === 0 ? (
           <div className="w-fit m-auto">Loading...</div>
         ) : (
@@ -202,12 +257,19 @@ const LobbyDetailModal = ({ isOpen, onClose, lobby, onJoin }) => {
                     {table.players_joined} / {lobby.max_players} players joined
                   </p>
                 </div>
-                <button
+                {!isOwner && (
+                  <button
                   onClick={() => handleJoinTable(table.id)}
                   className="bg-blue-500 text-white px-3 py-1 rounded hover:bg-blue-600"
-                >
+                  >
                   Join Table
-                </button>
+                </button>)}
+
+                {isOwner && (
+                  <p className={`border w-fit mt-1 px-3 py-2 rounded-full text-sm text-white ${visibilityColor(table)}`}>
+                  {table.status.toLowerCase().replaceAll("_", " ")}
+                </p>
+                )}
               </div>
             ))}
           </div>
